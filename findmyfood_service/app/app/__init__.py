3831ec3196d55ef3e675a1330c30fe75987eb5e4
Yonatan Gottesman <yonigo10@gmail.com>
2020-03-26 11:02:33 +0200

    delete old name

--- findmyfood_service/app/app/__init__.py
+++
@@ -1,5 +0,0 @@
-from starlette.applications import Starlette
-
-app = Starlette(debug=True)
-
-from app import routes
--- findmyfood_service/app/main.py
+++
@@ -1,7 +0,0 @@
-from app import app
-
-# The following code for debugging the app localy,
-# I dont need it for running with docker
-import uvicorn
-if __name__ == "__main__":
-    uvicorn.run(app, host='0.0.0.0', port=8000)
